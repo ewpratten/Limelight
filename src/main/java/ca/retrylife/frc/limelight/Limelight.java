32bad977cd6d3b37daaa55651222b3a32dcd0da8
Evan Pratten <ewpratten@gmail.com>
2020-01-25 20:07:30 -0500

    Added Contour

--- src/main/java/ca/retrylife/frc/limelight/Limelight.java
+++ src/main/java/ca/retrylife/frc/limelight/Limelight.java
@@ -1,5 +1,14 @@
 package ca.retrylife.frc.limelight;
 
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+
 public class Limelight {
-    
+
+    /* Camera data */
+    private double tv, tx, ty, ta, ts, tl, tshort, tlong, thor, tvert;
+
+    public Rotation2d getXRotation() {
+        return null;
+    }
+
 }

bdacfd0fb2e7b678597ba94273618bb30964a4cc
Evan Pratten <ewpratten@gmail.com>
2020-01-25 20:37:07 -0500

    reflect LEDMode to c

--- src/main/native/include/controls/LEDMode.h
+++ src/main/native/include/controls/LEDMode.h
@@ -1,5 +1,13 @@
 #pragma once
 
-namespace retrylife::frc{
-    
-}
+namespace retrylife::frc {
+/**
+ * Limelight LED mode setting
+ */
+enum LEDMode {
+    DEFAULT,  // Use pipeline default setting
+    OFF,      // Force-enable LEDs
+    BLINK,    // Force-disable LEDs
+    ON,       // Blink LEDs
+};
+}  // namespace retrylife::frc
